9ac467b6f392760b19bd5014bb08423597226e6b
Paul Mohr <p.mohr@sopamo.de>
2025-06-26 13:51:26 +0300

    Update download url

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,30 +2,28 @@ import * as os from 'os'
 import * as util from 'util'
 
 export function getKubectlArch(): string {
-   const arch = os.arch()
-   if (arch === 'x64') {
-      return 'amd64'
-   }
-   return arch
+  const arch = os.arch()
+  if (arch === 'x64') {
+    return 'amd64'
+  }
+  return arch
 }
 
-export function getkubectlDownloadURL(version: string, arch: string): string {
-   switch (os.type()) {
-      case 'Linux':
-         return `https://dl.k8s.io/release/${version}/bin/linux/${arch}/kubectl`
+export function getKubectlDownloadURL(version: string, arch: string): string {
+  const base = 'https://storage.googleapis.com/kubernetes-release'
+  switch (os.type()) {
+    case 'Linux':
+      return `${base}/release/${version}/bin/linux/${arch}/kubectl`
 
-      case 'Darwin':
-         return `https://dl.k8s.io/release/${version}/bin/darwin/${arch}/kubectl`
+    case 'Darwin':
+      return `${base}/release/${version}/bin/darwin/${arch}/kubectl`
 
-      case 'Windows_NT':
-      default:
-         return `https://dl.k8s.io/release/${version}/bin/windows/${arch}/kubectl.exe`
-   }
+    case 'Windows_NT':
+    default:
+      return `${base}/release/${version}/bin/windows/${arch}/kubectl.exe`
+  }
 }
 
 export function getExecutableExtension(): string {
-   if (os.type().match(/^Win/)) {
-      return '.exe'
-   }
-   return ''
+  return os.type().match(/^Win/) ? '.exe' : ''
 }
